3db5292b0d99c4e7730163c23a518339a75b5661
TRedBr <colin-brandes@gmx.de>
2026-01-28 10:34:04 +0100

    Added Alteration and Lookat
    Integrated them as well into movement

--- Movement_map.py
+++ Movement_map.py
@@ -9,32 +9,32 @@ def resetmap(ListofXcor, ListofAttributes,Ucordinate,XCor,YCor,Size):
     ListofXcor.clear()
     Size.update({"XCor":XCor, "YCor":YCor})
     i = 0
-    while i < XCor-1:
+    while i < XCor:
         ListofXcor.append(["_"])
         #ListofAttributes.append(["0"])
         j = 0
-        while j < YCor-1:
+        while j < YCor:
             ListofXcor[i].append("_")
             #ListofAttributes[j].append("0")
             j += 1
         i +=1
-    Ucordinate.update({"XCor":round((XCor-1)/2, 0), "YCor":round((YCor-1)/2, 0)})
+    Ucordinate.update({"XCor":round((XCor)/2, 0), "YCor":round((YCor)/2, 0)})
 
 def printmap(ListofXcor, Ucordinate, Size):
-    Mapwithuser = copy.deepcopy(ListofXcor)
+    Mapwithuser = copy.deepcopy(ListofXcor)                                                                             #using a temp map since exp. map size is relative small
     i = 0
     x = int(Ucordinate["XCor"])
     y = int(Ucordinate["YCor"])
     Mapwithuser[x][y] = "X"
-    while i < Size["XCor"]-1:
+    while i < Size["XCor"]:
         b = str(Mapwithuser[i]).replace("[","").replace("]","").replace(" ","").replace(",","").replace("'","")
-        print(f"{b}\n")
+        print(f"{b}")
         i += 1
     Mapwithuser.clear()
 
 
 def changeuserCor(Size,Ucordinate,NewXCor,NewYCor):
-    if int(NewXCor) < Size["XCor"] and int(NewYCor) < Size["YCor"]:
+    if int(NewXCor) < Size["XCor"]-1 and int(NewYCor) < Size["YCor"]:
         Ucordinate.update({"XCor":NewXCor, "YCor":NewYCor})
         print(f"User has been moved to X:{NewXCor}, Y:{NewYCor}")
     else:
@@ -48,7 +48,7 @@ def moveup(Size,Ucordinate):
         Ucordinate.update({"XCor": a-1})
 
 def movedown(Size,Ucordinate):
-    if Ucordinate["XCor"] == Size["XCor"]-2:
+    if Ucordinate["XCor"] == Size["XCor"]-1:
         print("Cant move Down")
     else:
         a = Ucordinate["XCor"]
@@ -63,49 +63,77 @@ def moveleft(Size,Ucordinate):
         Ucordinate.update({"YCor" : int(a-1)})
 
 def moveright(Size,Ucordinate):
-    if Ucordinate["YCor"] == Size["YCor"]-1:
+    if Ucordinate["YCor"] == Size["YCor"]:
         print("Cant move right")
     else:
         a = Ucordinate["YCor"]
         Ucordinate.update({"YCor": int(a+1)})
 
-def movementinterface(Size,Ucordinate):
+def movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes):
     x = input("Directional keys are W/S/A/D enter T to pause one turn, Exit to end the Program")
     match x.upper():
         case "W":
             moveup(Size,Ucordinate)
             printmap(ListofXcor, Ucordinate, Size)
-            movementinterface(Size, Ucordinate)
+            print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
+            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
         case "S":
             movedown(Size,Ucordinate)
             printmap(ListofXcor, Ucordinate, Size)
-            movementinterface(Size, Ucordinate)
+            print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
+            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
         case "A":
             moveleft(Size,Ucordinate)
             printmap(ListofXcor, Ucordinate, Size)
-            movementinterface(Size, Ucordinate)
+            print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
+            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
         case "D":
             moveright(Size,Ucordinate)
             printmap(ListofXcor, Ucordinate, Size)
-            print(Ucordinate)
-            movementinterface(Size, Ucordinate)
+            print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
+            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
         case "T":
             printmap(ListofXcor, Ucordinate, Size)
-            print("You Rested for one turn")
-            movementinterface(Size,Ucordinate)
+            print(f"You Rested for one turn at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
+            movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
         case "EXIT":
             print("Bye~")
         case _:
             print("Not a valid input, try again")
-            movementinterface(Size,Ucordinate)
+            movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
+
+def Alterlocation(Size,ListofXcor,ListofAttributes,XCor,YCor,NewTerrainType):
+    if XCor > Size["XCor"] or XCor < 0:
+        print("X Coordinate has to within the confines of the map ")
+    else:
+        if YCor > Size["YCor"] or YCor < 0:
+            print("Y Coordinate has to within the confines of the map ")
+        else:
+            print(f"Tile at X: {XCor}, Y: {YCor} has been changed from {ListofXcor[XCor][YCor]} to {NewTerrainType}")
+            ListofXcor[XCor][YCor] = NewTerrainType
+            i = 0
+            while i < len(ListofXcor):
+                print(str(ListofXcor[i]).replace("[","").replace("]","").replace(" ","").replace(",","").replace("'",""))
+                i += 1
+
+def Lookat(Size,ListofXcor,ListofAttributes,XCor,YCor):
+    if XCor > Size["XCor"] or XCor < 0:
+        print("X Coordinate to look at has to within the confines of the map ")
+    else:
+        if YCor > Size["YCor"] or YCor < 0:
+            print("Y Coordinate to look at has to within the confines of the map ")
+        else:
+            return ListofXcor[XCor][YCor]
 
 
 
-
-resetmap(ListofXcor, ListofAttributes, Ucordinate,6,7, Size)
+resetmap(ListofXcor, ListofAttributes, Ucordinate,9,9, Size)
 printmap(ListofXcor, Ucordinate, Size)
+Alterlocation(Size,ListofXcor,ListofAttributes,4,5,"A")
+Alterlocation(Size,ListofXcor,ListofAttributes,4,2,"T")
+print(f" You see {Lookat(Size,ListofXcor,ListofAttributes,4,5)} at X:4 Y:5")
 # print(Ucordinate)
-# changeuserCor(Size,Ucordinate,3,17)
+# changeuserCor(Size,Ucordinate,3,7)
 # printmap(ListofXcor,Ucordinate, Size)
 # print(Ucordinate)
 # changeuserCor(Size,Ucordinate,4,9)
@@ -116,4 +144,4 @@ printmap(ListofXcor, Ucordinate, Size)
 #     print(f"{ListofXcor[k]}\n")
 #     k += 1
 # print(Ucordinate)
-movementinterface(Size,Ucordinate)
+movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
