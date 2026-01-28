1e3585569cbd1767d45465f25d2aab28c16900a6
TRedBr <colin-brandes@gmx.de>
2026-01-28 11:36:45 +0100

    Cleanup and Overview

--- Movement_map.py
+++ Movement_map.py
@@ -5,6 +5,8 @@ ListofAttributes = []
 Ucordinate = {"XCor":0 , "YCor":0}
 Size = {"XCor":0 , "YCor":0}
 
+
+
 def resetmap(ListofXcor, ListofAttributes,Ucordinate,XCor,YCor,Size):
     ListofXcor.clear()
     Size.update({"XCor":XCor, "YCor":YCor})
@@ -20,6 +22,8 @@ def resetmap(ListofXcor, ListofAttributes,Ucordinate,XCor,YCor,Size):
         i +=1
     Ucordinate.update({"XCor":round((XCor)/2, 0), "YCor":round((YCor)/2, 0)})
 
+
+
 def printmap(ListofXcor, Ucordinate, Size):
     Mapwithuser = copy.deepcopy(ListofXcor)                                                                             #using a temp map since exp. map size is relative small
     i = 0
@@ -33,12 +37,15 @@ def printmap(ListofXcor, Ucordinate, Size):
     Mapwithuser.clear()
 
 
+
 def changeuserCor(Size,Ucordinate,NewXCor,NewYCor):
     if int(NewXCor) < Size["XCor"]-1 and int(NewYCor) < Size["YCor"]:
         Ucordinate.update({"XCor":NewXCor, "YCor":NewYCor})
         print(f"User has been moved to X:{NewXCor}, Y:{NewYCor}")
     else:
         print(f"Cant move User to X:{NewXCor}, Y:{NewYCor} because its outside the map")
+
+
 
 def moveup(Size,Ucordinate,ListofAttributes):
     if Ucordinate["XCor"] == 0:
@@ -49,6 +56,8 @@ def moveup(Size,Ucordinate,ListofAttributes):
         else:
             a = Ucordinate["XCor"]
             Ucordinate.update({"XCor": a-1})
+
+
 
 def movedown(Size,Ucordinate,ListofAttributes):
     if Ucordinate["XCor"] == Size["XCor"]-1:
@@ -61,6 +70,7 @@ def movedown(Size,Ucordinate,ListofAttributes):
             Ucordinate.update({"XCor" : a+1 })
 
 
+
 def moveleft(Size,Ucordinate,ListofAttributes):
     if Ucordinate["YCor"] == 0:
         print("Cant move left")
@@ -71,6 +81,8 @@ def moveleft(Size,Ucordinate,ListofAttributes):
             a = Ucordinate["YCor"]
             Ucordinate.update({"YCor" : int(a-1)})
 
+
+
 def moveright(Size,Ucordinate,ListofAttributes):
     if Ucordinate["YCor"] == Size["YCor"]:
         print("Cant move right")
@@ -80,6 +92,8 @@ def moveright(Size,Ucordinate,ListofAttributes):
         else:
             a = Ucordinate["YCor"]
             Ucordinate.update({"YCor": int(a+1)})
+
+
 
 def movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes):
     x = input("Directional keys are \033[0;32mW/S/A/D\033[0m enter \033[0;34mT\033[0m to pause one turn, \033[0;31mExit\033[0m to end the Program")
@@ -114,6 +128,8 @@ def movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes):
             print("Not a valid input, try again")
             movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
 
+
+
 def Alterlocation(Size,ListofXcor,ListofAttributes,XCor,YCor,NewTerrainType):
     if XCor > Size["XCor"] or XCor < 0:
         print("X Coordinate has to within the confines of the map ")
@@ -129,6 +145,8 @@ def Alterlocation(Size,ListofXcor,ListofAttributes,XCor,YCor,NewTerrainType):
                 print(str(ListofXcor[i]).replace("[","").replace("]","").replace(" ","").replace(",","").replace("'",""))
                 i += 1
 
+
+
 def Lookat(Size,ListofXcor,ListofAttributes,XCor,YCor):
     if XCor > Size["XCor"] or XCor < 0:
         print("X Coordinate to look at has to within the confines of the map ")
@@ -137,6 +155,8 @@ def Lookat(Size,ListofXcor,ListofAttributes,XCor,YCor):
             print("Y Coordinate to look at has to within the confines of the map ")
         else:
             return ListofXcor[XCor][YCor]
+
+
 
 def UpdateAttributelist(Size,ListofXcor,ListofAttributes):
     i = 0
