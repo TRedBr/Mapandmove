292814b84152bb4932926e246ca2c75b46875bd3
TRedBr <colin-brandes@gmx.de>
2026-01-29 12:41:36 +0100

    reading-data-problem-WIP-might-have-to-rethink

--- Datamanagement_Map.py
+++ Datamanagement_Map.py
@@ -1,20 +1,100 @@
-import os.path                              #planning to use in checkmapfile(Mapname)
+import os
+import os.path
 
-Dummylist = [["_","_","_"],["_","_","_"],["_","_","_"]]
-Dummyattributelist = [["0","0","0"],["0","0","0"],["0","0","0"]]
+dummylist = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+dummyattributelist = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]
+dummycor = {"XCor": 1,"YCor":2}
 
-def newsavemap(Maplist, Mapattributelist, Mapname):
-    with open(f"mapdata/{Mapname}.txt", 'at') as Mapfile:
+
+def newsavemap(maplist, mapattributelist, ucordinate, mapname):
+    with open(f"mapdata/{mapname}.txt", 'at') as mapfile:
         i = 0
-        while i < len(Maplist):
-            Mapfile.write(f"{str(Maplist[i]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
+        while i < len(maplist):
+            mapfile.write(f"{str(maplist[i]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
             i += 1
-        Mapfile.write("Atr\n")                          #divider for later read for Mapattributelist
+        mapfile.write("Atr\n")  # divider for later read for mapattributelist
         j = 0
-        while j < len(Mapattributelist):
-            Mapfile.write(f"{str(Mapattributelist[j]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
+        while j < len(mapattributelist):
+            mapfile.write(
+                f"{str(mapattributelist[j]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
             j += 1
+        mapfile.write("User\n")
+        mapfile.write(f"{str(ucordinate).replace("{", "").replace("}", "").replace(" ", "").replace("'", "")}\n")
+        mapfile.write("End")  # End point usable to stop reading
+        mapfile.close()
 
-newsavemap(Dummylist, Dummyattributelist, "dummylist")
 
-#def checkmapfile(Mapname):             #used to check if mapfile exists used before savemap, returns True of False
+def checkmapfile(mapname):  # gives out true or false if .txt file with inserted variable as name exists
+    if os.path.exists(
+            f"mapdata/{mapname}.txt"):  # used before saving and reading out save files to make sure they are there
+        return True
+    else:
+        return False
+
+
+def overwritemapfile(maplist, mapattributelist, mapname):
+    with open(f"mapdata/{mapname}.txt", 'wt') as mapfile:
+        i = 0
+        while i < len(maplist):
+            mapfile.write(f"{str(maplist[i]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
+            i += 1
+        mapfile.write("Atr\n")  # divider for later read for mapattributelist
+        j = 0
+        while j < len(mapattributelist):
+            mapfile.write(
+                f"{str(mapattributelist[j]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")}\n")
+            j += 1
+        mapfile.write("User\n")
+        mapfile.write(f"{str(ucordinate).replace("{", "").replace("}", "").replace(" ", "").replace("'", "")}\n")
+        mapfile.write("End")  # End point usable to stop reading
+        mapfile.close()
+
+
+# def loadmapfile(maplist, mapattributelist, size, ucordinates, mapname):
+#     maplist = []
+#     mapattributelist = []
+#     size.update({"XCor":0,"YCor":0})
+#     ucordinates.update({"XCor":0,"YCor":0})
+#     with open(f"mapdata/{mapname}.txt", 'rt') as mapfile:
+#         listfile = mapfile.read().splitlines()
+#         loopcounter = 0
+#         xlenght = 0
+#         ylenght = 0
+#         for entry in listfile:
+#             if entry == 'Atr':
+#                 loopcounter = 0
+#                 break
+#             elif entry == 'End':
+#                 loopcounter = 0
+#                 break
+#             elif loopcounter == 255:
+#                 loopcounter = 0
+#                 break
+#             else:
+#                 xlenght += 1
+#                 loopcounter += 1
+#         for entry in listfile:
+#             if loopcounter == 0:
+#                 firstline = str(entry).replace("[", "").replace("]", "").replace(" ", "").replace("'", "").replace(",","")
+#                 ylenght = len(firstline)
+#                 loopcounter += 1
+#             else:
+#                 loopcounter = 0
+#                 break
+#         size.update({"XCor":xlenght,"YCor":ylenght})
+#         i = 0
+#         j = 0
+#         while i < xlenght:
+#             templist = []
+#             while j < ylenght:     # WIP questionable if this long code makes sense for this might have to redo save file structure for easier read access
+
+
+
+# controleblock
+# newsavemap(dummylist, dummyattributelist, dummycor, "dummylist")
+
+# with open(f"mapdata/dummylist.txt", 'rt') as mapfile:
+#     listfile = mapfile.read().splitlines()
+#     print(listfile)
+
+# def checkmapfile(mapname):             #used to check if mapfile exists used before savemap, returns True of False
--- Movement_map.py
+++ Movement_map.py
@@ -1,15 +1,16 @@
 import copy
 
-ListofXcor = []
-ListofAttributes = []
-Ucordinate = {"XCor":0 , "YCor":0}
-Size = {"XCor":0 , "YCor":0}
+ListofXcor = []  # list of lists of objects showing the visuals of the ma
+ListofAttributes = []  # list of lists of secondary variables for specific locations on the map
+Ucordinate = {"XCor": 0, "YCor": 0}  # user coordinates used to id the user location without altering the map
+Size = {"XCor": 0, "YCor": 0}  # Size of the map, for simpler access to some parameters
 
 
-
-def resetmap(ListofXcor, ListofAttributes,Ucordinate,XCor,YCor,Size):
-    ListofXcor.clear()
-    Size.update({"XCor":XCor, "YCor":YCor})
+def resetmap(ListofXcor, ListofAttributes, Ucordinate, XCor, YCor,
+             Size):  # resets the map to specific parameters, all with 'Empty' tiles both for the map as well as the attribute map
+    ListofXcor.clear()  # also used to generate a new empty map
+    Size.update(
+        {"XCor": XCor, "YCor": YCor})  # requires XCor and YCor to be int - make sure to test input for correct input
     i = 0
     while i < XCor:
         ListofXcor.append(["_"])
@@ -19,35 +20,38 @@ def resetmap(ListofXcor, ListofAttributes,Ucordinate,XCor,YCor,Size):
             ListofXcor[i].append("_")
             ListofAttributes[i].append("0")
             j += 1
-        i +=1
-    Ucordinate.update({"XCor":round((XCor)/2, 0), "YCor":round((YCor)/2, 0)})
+        i += 1
+    Ucordinate.update({"XCor": round((XCor) / 2, 0),
+                       "YCor": round((YCor) / 2, 0)})  # by choice the default places the user in the center
 
 
-
-def printmap(ListofXcor, Ucordinate, Size):
-    Mapwithuser = copy.deepcopy(ListofXcor)                                                                             #using a temp map since exp. map size is relative small
+def printmap(ListofXcor, Ucordinate,
+             Size):  # outputs a print of the map with the user placed in it, without altering the main map
+    Mapwithuser = copy.deepcopy(ListofXcor)  # using a temp map since exp. map size is relative small
     i = 0
-    x = int(Ucordinate["XCor"])
-    y = int(Ucordinate["YCor"])
-    Mapwithuser[x][y] = "X"
+    userCorX = int(Ucordinate["XCor"])
+    userCorY = int(Ucordinate["YCor"])
+    Mapwithuser[userCorX][userCorY] = "X"
     while i < Size["XCor"]:
-        b = str(Mapwithuser[i]).replace("[","").replace("]","").replace(" ","").replace(",","").replace("'","")
+        b = str(Mapwithuser[i]).replace("[", "").replace("]", "").replace(" ", "").replace(",", "").replace("'",
+                                                                                                            "")  # cleans the map to an easier to read space
         print(f"{b}")
         i += 1
     Mapwithuser.clear()
 
 
-
-def changeuserCor(Size,Ucordinate,NewXCor,NewYCor):
-    if int(NewXCor) < Size["XCor"]-1 and int(NewYCor) < Size["YCor"]:
-        Ucordinate.update({"XCor":NewXCor, "YCor":NewYCor})
+def changeuserCor(Size, Ucordinate, NewXCor,
+                  NewYCor):  # used to place the user on a new tile with cor NewXCor and NewYCor
+    if int(NewXCor) < Size["XCor"] - 1 and int(NewYCor) < Size["YCor"]:  # requres input to be int test before
+        Ucordinate.update({"XCor": NewXCor, "YCor": NewYCor})
         print(f"User has been moved to X:{NewXCor}, Y:{NewYCor}")
     else:
         print(f"Cant move User to X:{NewXCor}, Y:{NewYCor} because its outside the map")
 
 
+# Movementoptions of user
 
-def moveup(Size,Ucordinate,ListofAttributes):
+def moveup(Size, Ucordinate, ListofAttributes):
     if Ucordinate["XCor"] == 0:
         print("Cant move Up")
     else:
@@ -55,23 +59,21 @@ def moveup(Size,Ucordinate,ListofAttributes):
             print("Cant move Up")
         else:
             a = Ucordinate["XCor"]
-            Ucordinate.update({"XCor": a-1})
+            Ucordinate.update({"XCor": a - 1})
 
 
-
-def movedown(Size,Ucordinate,ListofAttributes):
-    if Ucordinate["XCor"] == Size["XCor"]-1:
+def movedown(Size, Ucordinate, ListofAttributes):
+    if Ucordinate["XCor"] == Size["XCor"] - 1:
         print("Cant move Down")
     else:
-        if ListofAttributes[int(Ucordinate["XCor"])+1][int(Ucordinate["YCor"])] == "X":
+        if ListofAttributes[int(Ucordinate["XCor"]) + 1][int(Ucordinate["YCor"])] == "X":
             print("Cant move Down")
         else:
             a = Ucordinate["XCor"]
-            Ucordinate.update({"XCor" : a+1 })
+            Ucordinate.update({"XCor": a + 1})
 
 
-
-def moveleft(Size,Ucordinate,ListofAttributes):
+def moveleft(Size, Ucordinate, ListofAttributes):
     if Ucordinate["YCor"] == 0:
         print("Cant move left")
     else:
@@ -79,58 +81,56 @@ def moveleft(Size,Ucordinate,ListofAttributes):
             print("Cant move left")
         else:
             a = Ucordinate["YCor"]
-            Ucordinate.update({"YCor" : int(a-1)})
+            Ucordinate.update({"YCor": int(a - 1)})
 
 
-
-def moveright(Size,Ucordinate,ListofAttributes):
+def moveright(Size, Ucordinate, ListofAttributes):
     if Ucordinate["YCor"] == Size["YCor"]:
         print("Cant move right")
     else:
-        if ListofAttributes[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])+1] == "X":
+        if ListofAttributes[int(Ucordinate["XCor"])][int(Ucordinate["YCor"]) + 1] == "X":
             print("Cant move right")
         else:
             a = Ucordinate["YCor"]
-            Ucordinate.update({"YCor": int(a+1)})
+            Ucordinate.update({"YCor": int(a + 1)})
 
 
-
-def movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes):
-    x = input("Directional keys are \033[0;32mW/S/A/D\033[0m enter \033[0;34mT\033[0m to pause one turn, \033[0;31mExit\033[0m to end the Program")
+def movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes):
+    x = input(
+        "Directional keys are \033[0;32mW/S/A/D\033[0m enter \033[0;34mT\033[0m to pause one turn, \033[0;31mExit\033[0m to end the Program")
     match x.upper():
         case "W":
-            moveup(Size,Ucordinate,ListofAttributes)
+            moveup(Size, Ucordinate, ListofAttributes)
             printmap(ListofXcor, Ucordinate, Size)
             print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
-            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
         case "S":
-            movedown(Size,Ucordinate,ListofAttributes)
+            movedown(Size, Ucordinate, ListofAttributes)
             printmap(ListofXcor, Ucordinate, Size)
             print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
-            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
         case "A":
-            moveleft(Size,Ucordinate,ListofAttributes)
+            moveleft(Size, Ucordinate, ListofAttributes)
             printmap(ListofXcor, Ucordinate, Size)
             print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
-            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
         case "D":
-            moveright(Size,Ucordinate,ListofAttributes)
+            moveright(Size, Ucordinate, ListofAttributes)
             printmap(ListofXcor, Ucordinate, Size)
             print(f"You are at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
-            movementinterface(Size, Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
         case "T":
             printmap(ListofXcor, Ucordinate, Size)
             print(f"You Rested for one turn at a {ListofXcor[int(Ucordinate["XCor"])][int(Ucordinate["YCor"])]}")
-            movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
         case "EXIT":
             print("Bye~")
         case _:
             print("Not a valid input, try again")
-            movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
+            movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
 
 
-
-def Alterlocation(Size,ListofXcor,ListofAttributes,XCor,YCor,NewTerrainType):
+def Alterlocation(Size, ListofXcor, ListofAttributes, XCor, YCor, NewTerrainType):
     if XCor > Size["XCor"] or XCor < 0:
         print("X Coordinate has to within the confines of the map ")
     else:
@@ -140,14 +140,15 @@ def Alterlocation(Size,ListofXcor,ListofAttributes,XCor,YCor,NewTerrainType):
             print(f"Tile at X: {XCor}, Y: {YCor} has been changed from {ListofXcor[XCor][YCor]} to {NewTerrainType}")
             ListofXcor[XCor][YCor] = NewTerrainType
             i = 0
-            UpdateAttributelist(Size,ListofXcor,ListofAttributes)
+            UpdateAttributelist(Size, ListofXcor, ListofAttributes)
             while i < len(ListofXcor):
-                print(str(ListofXcor[i]).replace("[","").replace("]","").replace(" ","").replace(",","").replace("'",""))
+                print(
+                    str(ListofXcor[i]).replace("[", "").replace("]", "").replace(" ", "").replace(",", "").replace("'",
+                                                                                                                   ""))
                 i += 1
 
 
-
-def Lookat(Size,ListofXcor,ListofAttributes,XCor,YCor):
+def Lookat(Size, ListofXcor, ListofAttributes, XCor, YCor):
     if XCor > Size["XCor"] or XCor < 0:
         print("X Coordinate to look at has to within the confines of the map ")
     else:
@@ -157,8 +158,7 @@ def Lookat(Size,ListofXcor,ListofAttributes,XCor,YCor):
             return ListofXcor[XCor][YCor]
 
 
-
-def UpdateAttributelist(Size,ListofXcor,ListofAttributes):
+def UpdateAttributelist(Size, ListofXcor, ListofAttributes):
     i = 0
     while i < len(ListofXcor):
         j = 0
@@ -183,20 +183,19 @@ def UpdateAttributelist(Size,ListofXcor,ListofAttributes):
         i += 1
 
 
-
-
-resetmap(ListofXcor, ListofAttributes, Ucordinate,9,9, Size)
+###Controle Block
+resetmap(ListofXcor, ListofAttributes, Ucordinate, 9, 19, Size)
 printmap(ListofXcor, Ucordinate, Size)
-Alterlocation(Size,ListofXcor,ListofAttributes,4,5,"A")
-Alterlocation(Size,ListofXcor,ListofAttributes,4,2,"T")
-Alterlocation(Size,ListofXcor,ListofAttributes,0,0,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,0,1,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,0,2,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,1,0,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,1,1,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,1,2,"~")
-Alterlocation(Size,ListofXcor,ListofAttributes,3,6,"B")
-print(f" You see {Lookat(Size,ListofXcor,ListofAttributes,4,5)} at X:4 Y:5")
+Alterlocation(Size, ListofXcor, ListofAttributes, 4, 5, "A")
+Alterlocation(Size, ListofXcor, ListofAttributes, 4, 2, "T")
+Alterlocation(Size, ListofXcor, ListofAttributes, 0, 0, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 0, 1, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 0, 2, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 1, 0, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 1, 1, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 1, 2, "~")
+Alterlocation(Size, ListofXcor, ListofAttributes, 3, 6, "B")
+print(f" You see {Lookat(Size, ListofXcor, ListofAttributes, 4, 5)} at X:4 Y:5")
 # UpdateAttributelist(Size,ListofXcor,ListofAttributes)
 # print(Ucordinate)
 # changeuserCor(Size,Ucordinate,3,7)
@@ -205,13 +204,13 @@ print(f" You see {Lookat(Size,ListofXcor,ListofAttributes,4,5)} at X:4 Y:5")
 # changeuserCor(Size,Ucordinate,4,9)
 # printmap(ListofXcor,Ucordinate, Size)
 # print(Ucordinate)
-# k = 0
-# while k < Size["XCor"]-1:
-#     print(f"{ListofXcor[k]}\n")
-#     k += 1
-# l = 0
-# while l < Size["XCor"]:
-#     print(f"{ListofAttributes[l]}\n")
-#     l += 1
+k = 0
+while k < Size["XCor"] - 1:
+    print(f"{ListofXcor[k]}\n")
+    k += 1
+l = 0
+while l < Size["XCor"]:
+    print(f"{ListofAttributes[l]}\n")
+    l += 1
 # print(Ucordinate)
-movementinterface(Size,Ucordinate,ListofXcor,ListofAttributes)
+movementinterface(Size, Ucordinate, ListofXcor, ListofAttributes)
